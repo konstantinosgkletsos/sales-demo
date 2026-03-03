d8d5dc35860c6099625168b42c1b9db301f8c080
konstantinosgkletsos <svigis2001@gmail.com>
2026-03-03 14:44:24 +0200

    Updated Report Title again

--- sales_pipeline_demo.py
+++ sales_pipeline_demo.py
@@ -8,7 +8,7 @@ Run:
 from collections import defaultdict
 
 ENVIRONMENT = "dev"
-REPORT_TITLE = "Sales Pipeline Demo By Country"
+REPORT_TITLE = "Sales Pipeline Demo By European Country"
 
 
 def extract_raw_orders():
